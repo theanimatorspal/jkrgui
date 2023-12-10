e57c98986026d75560d870a3df425be0d038ebf4
sansk <sansk@KOIRALASANSKRIT>
2023-12-10 17:34:37 +0545

    removed stuff

--- out/build/x64-release with debug/aJkrLua/Config.lua
+++
@@ -1,8 +0,0 @@
-Theme = {}
-Theme.Colors = {}
-Theme.Colors.Button = {}
-Theme.Colors.Button.Normal = vec4(0.5, 0.5, 0.5, 1)
-Theme.Colors.Button.Hover = vec4(0.5, 0.5, 0.5, 1)
-Theme.Colors.Shadow = vec4(0, 0, 0, 0.2)
-Theme.Colors.Text = {}
-Theme.Colors.Text.Normal = vec4(0, 0, 0, 1)
