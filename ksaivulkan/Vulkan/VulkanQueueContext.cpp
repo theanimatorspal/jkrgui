0edfe0b879540a283dc9c2ea3298f7c4dc7514ad
Darshan Koirala <theanimatorspal@gmail.com>
2024-04-19 15:21:34 +0545

    removed CGLM

--- ksaivulkan/Vulkan/VulkanQueueContext.cpp
+++ ksaivulkan/Vulkan/VulkanQueueContext.cpp
@@ -2,66 +2,58 @@
 #include "VulkanCommandBuffer.hpp"
 #include "VulkanFence.hpp"
 #include "VulkanSemaphore.hpp"
-#include <cglm/cglm.h>
 class VulkanSemaphore;
 class VulkanFence;
 class VulkanCommandBuffer;
 using namespace ksai;
 
-VulkanQueueContext::VulkanQueueContext(const VulkanInstance& inInstance, const VulkanPhysicalDevice& inDevice)
-    : mPhysicalDevice(inDevice.GetPhysicalDeviceHandle())
-{
-    SDL_Window* Window = SDL_CreateWindow("Test", 0, 0, 100, 100, SDL_WINDOW_HIDDEN | SDL_WINDOW_VULKAN);
-    VulkanSurface Surface(inInstance, Window);
-    auto SurfaceHandle = Surface.GetSurfaceHandle();
-    mQueueFamilyProperties = mPhysicalDevice.getQueueFamilyProperties();
-    auto PropertyIterator = std::find_if(mQueueFamilyProperties.begin(), mQueueFamilyProperties.end(),
-        [](vk::QueueFamilyProperties const& qfp) {
-            return (qfp.queueFlags & vk::QueueFlagBits::eGraphics);
-        });
+VulkanQueueContext::VulkanQueueContext(const VulkanInstance& inInstance, const VulkanPhysicalDevice& inDevice) : mPhysicalDevice(inDevice.GetPhysicalDeviceHandle()) {
+               SDL_Window* Window = SDL_CreateWindow("Test", 0, 0, 100, 100, SDL_WINDOW_HIDDEN | SDL_WINDOW_VULKAN);
+               VulkanSurface Surface(inInstance, Window);
+               auto SurfaceHandle     = Surface.GetSurfaceHandle();
+               mQueueFamilyProperties = mPhysicalDevice.getQueueFamilyProperties();
+               auto PropertyIterator  = std::find_if(
+                         mQueueFamilyProperties.begin(), mQueueFamilyProperties.end(), [](vk::QueueFamilyProperties const& qfp) { return (qfp.queueFlags & vk::QueueFlagBits::eGraphics); });
 
-    mGraphicsQueueIndex = std::distance(mQueueFamilyProperties.begin(), PropertyIterator);
-    assert(mGraphicsQueueIndex < mQueueFamilyProperties.size());
+               mGraphicsQueueIndex = std::distance(mQueueFamilyProperties.begin(), PropertyIterator);
+               assert(mGraphicsQueueIndex < mQueueFamilyProperties.size());
 
-    size_t presentQueueFamilyIndex = mPhysicalDevice .getSurfaceSupportKHR(mGraphicsQueueIndex, SurfaceHandle)
-        ? mGraphicsQueueIndex
-        : mQueueFamilyProperties.size();
-    mPresentQueueIndex = presentQueueFamilyIndex;
+               size_t presentQueueFamilyIndex = mPhysicalDevice.getSurfaceSupportKHR(mGraphicsQueueIndex, SurfaceHandle) ? mGraphicsQueueIndex : mQueueFamilyProperties.size();
+               mPresentQueueIndex             = presentQueueFamilyIndex;
 
-    if (presentQueueFamilyIndex == mQueueFamilyProperties.size()) {
-        for (int i = 0; i < mQueueFamilyProperties.size(); i++) {
-            bool isQueueSupportedForPresentAndGraphics = (mQueueFamilyProperties[i].queueFlags & vk::QueueFlagBits::eGraphics) && mPhysicalDevice.getSurfaceSupportKHR(mGraphicsQueueIndex, SurfaceHandle);
-            if (isQueueSupportedForPresentAndGraphics) {
-                mGraphicsQueueIndex = i;
-                mPresentQueueIndex = i;
-                break;
-            }
-        }
+               if (presentQueueFamilyIndex == mQueueFamilyProperties.size()) {
+                              for (int i = 0; i < mQueueFamilyProperties.size(); i++) {
+                                             bool isQueueSupportedForPresentAndGraphics = (mQueueFamilyProperties[i].queueFlags & vk::QueueFlagBits::eGraphics) &&
+                                                                                          mPhysicalDevice.getSurfaceSupportKHR(mGraphicsQueueIndex, SurfaceHandle);
+                                             if (isQueueSupportedForPresentAndGraphics) {
+                                                            mGraphicsQueueIndex = i;
+                                                            mPresentQueueIndex  = i;
+                                                            break;
+                                             }
+                              }
 
-        bool isPresentQueueDifferent = (mPresentQueueIndex == mQueueFamilyProperties.size());
-        if (isPresentQueueDifferent) {
-            for (int i = 0; i < mQueueFamilyProperties.size(); i++) {
-                bool SupportsPresentation = mPhysicalDevice.getSurfaceSupportKHR(i, SurfaceHandle);
-                if (SupportsPresentation) {
-                    mPresentQueueIndex = i;
-                    break;
-                }
-            }
-        }
-    }
-    bool NoQueueForGraphicsOrPresent = (mGraphicsQueueIndex == mQueueFamilyProperties.size()) || (mPresentQueueIndex == mQueueFamilyProperties.size());
-    ksai_print("GraphicsQueue Index:");
-    ksai_print(std::to_string(mGraphicsQueueIndex).c_str());
-    ksai_print("PresentQueueIndex:");
-    ksai_print(std::to_string(mPresentQueueIndex).c_str());
+                              bool isPresentQueueDifferent = (mPresentQueueIndex == mQueueFamilyProperties.size());
+                              if (isPresentQueueDifferent) {
+                                             for (int i = 0; i < mQueueFamilyProperties.size(); i++) {
+                                                            bool SupportsPresentation = mPhysicalDevice.getSurfaceSupportKHR(i, SurfaceHandle);
+                                                            if (SupportsPresentation) {
+                                                                           mPresentQueueIndex = i;
+                                                                           break;
+                                                            }
+                                             }
+                              }
+               }
+               bool NoQueueForGraphicsOrPresent = (mGraphicsQueueIndex == mQueueFamilyProperties.size()) || (mPresentQueueIndex == mQueueFamilyProperties.size());
+               ksai_print("GraphicsQueue Index:");
+               ksai_print(std::to_string(mGraphicsQueueIndex).c_str());
+               ksai_print("PresentQueueIndex:");
+               ksai_print(std::to_string(mPresentQueueIndex).c_str());
 
-    if (NoQueueForGraphicsOrPresent) {
-        ksai_print("No Queue For Graphics Or Present");
-        throw std::runtime_error("Error : Count not find a suitable queue for graphics or present");
-    }
-    SDL_DestroyWindow(Window);
+               if (NoQueueForGraphicsOrPresent) {
+                              ksai_print("No Queue For Graphics Or Present");
+                              throw std::runtime_error("Error : Count not find a suitable queue for graphics or present");
+               }
+               SDL_DestroyWindow(Window);
 }
 
-VulkanQueueContext::~VulkanQueueContext()
-{
-}
+VulkanQueueContext::~VulkanQueueContext() {}
--- ksaivulkan/Vulkan/VulkanSurface.cpp
+++ ksaivulkan/Vulkan/VulkanSurface.cpp
@@ -5,62 +5,52 @@
 #else
 #include <SDL2/SDL_vulkan.h>
 #endif
-#include <cglm/cglm.h>
 #include <iostream>
 using namespace ksai;
 #undef max
 
-VulkanSurface::VulkanSurface(const VulkanInstance& inInstance, SDL_Window* inWindow)
-    : mInstance(inInstance.GetInstanceHandle())
-    , mWindow(inWindow)
-{
-    SDL_bool Error = SDL_Vulkan_CreateSurface(inWindow, inInstance.GetInstanceHandle(), (VkSurfaceKHR*)&mSurface);
-    if (Error == SDL_FALSE) {
-        ksai_print("Surface Creation Error");
-        std::cout << "Error Surface Create\n";
-    }
+VulkanSurface::VulkanSurface(const VulkanInstance& inInstance, SDL_Window* inWindow) : mInstance(inInstance.GetInstanceHandle()), mWindow(inWindow) {
+               SDL_bool Error = SDL_Vulkan_CreateSurface(inWindow, inInstance.GetInstanceHandle(), (VkSurfaceKHR*)&mSurface);
+               if (Error == SDL_FALSE) {
+                              ksai_print("Surface Creation Error");
+                              std::cout << "Error Surface Create\n";
+               }
 }
 
-VulkanSurface::~VulkanSurface()
-{
-    mInstance.destroySurfaceKHR(mSurface);
+VulkanSurface::~VulkanSurface() { mInstance.destroySurfaceKHR(mSurface); }
+
+VulkanSurface& VulkanSurface::ProcessCurrentSurfaceConditions(const VulkanPhysicalDevice& inPhysicalDevice, vk::PresentModeKHR inMode) {
+               ProcessCurrentSurfaceExtents(inPhysicalDevice);
+               mPresentMode = inMode;
+               mPreTransform =
+                         (mSurfaceCapabilities.supportedTransforms & vk::SurfaceTransformFlagBitsKHR::eIdentity) ? vk::SurfaceTransformFlagBitsKHR::eIdentity : mSurfaceCapabilities.currentTransform;
+
+               mCompositeAlpha = (mSurfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePreMultiplied)    ? vk::CompositeAlphaFlagBitsKHR::ePreMultiplied
+                                 : (mSurfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePostMultiplied) ? vk::CompositeAlphaFlagBitsKHR::ePostMultiplied
+                                 : (mSurfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::eInherit)        ? vk::CompositeAlphaFlagBitsKHR::eInherit
+                                                                                                                                   : vk::CompositeAlphaFlagBitsKHR::eOpaque;
+               return *this;
 }
 
-VulkanSurface& VulkanSurface::ProcessCurrentSurfaceConditions(const VulkanPhysicalDevice& inPhysicalDevice, vk::PresentModeKHR inMode)
-{
-    ProcessCurrentSurfaceExtents(inPhysicalDevice);
-    mPresentMode = inMode; 
-    mPreTransform = (mSurfaceCapabilities.supportedTransforms & vk::SurfaceTransformFlagBitsKHR::eIdentity)
-        ? vk::SurfaceTransformFlagBitsKHR::eIdentity
-        : mSurfaceCapabilities.currentTransform;
+VulkanSurface& VulkanSurface::ProcessCurrentSurfaceExtents(const VulkanPhysicalDevice& inPhysicalDevice) {
+               const auto& mPhysicalDevice               = inPhysicalDevice.GetPhysicalDeviceHandle();
+               std::vector<vk::SurfaceFormatKHR> formats = mPhysicalDevice.getSurfaceFormatsKHR(mSurface);
+               assert(!formats.empty());
 
-    mCompositeAlpha = (mSurfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePreMultiplied) ? vk::CompositeAlphaFlagBitsKHR::ePreMultiplied
-        : (mSurfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePostMultiplied)            ? vk::CompositeAlphaFlagBitsKHR::ePostMultiplied
-        : (mSurfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::eInherit)                   ? vk::CompositeAlphaFlagBitsKHR::eInherit
-                                                                                                                     : vk::CompositeAlphaFlagBitsKHR::eOpaque;
-    return *this;
-}
+               mSurfaceImageFormat = (formats[0].format == vk::Format::eUndefined) ? vk::Format::eB8G8R8A8Unorm : formats[0].format;
+               // TODO Make this better
+               mSurfaceImageFormat       = vk::Format::eB8G8R8A8Unorm;
 
-VulkanSurface& VulkanSurface::ProcessCurrentSurfaceExtents(const VulkanPhysicalDevice& inPhysicalDevice)
-{
-    const auto& mPhysicalDevice = inPhysicalDevice.GetPhysicalDeviceHandle();
-    std::vector<vk::SurfaceFormatKHR> formats = mPhysicalDevice.getSurfaceFormatsKHR(mSurface);
-    assert(!formats.empty());
+               mSurfaceCapabilities      = mPhysicalDevice.getSurfaceCapabilitiesKHR(mSurface);
+               bool surfaceSizeUndefined = mSurfaceCapabilities.currentExtent.width == std::numeric_limits<uint32_t>::max();
 
-    mSurfaceImageFormat = (formats[0].format == vk::Format::eUndefined) ? vk::Format::eB8G8R8A8Unorm : formats[0].format;
-    // TODO Make this better
-    mSurfaceImageFormat = vk::Format::eB8G8R8A8Unorm;
-
-    mSurfaceCapabilities = mPhysicalDevice.getSurfaceCapabilitiesKHR(mSurface);
-    bool surfaceSizeUndefined = mSurfaceCapabilities.currentExtent.width == std::numeric_limits<uint32_t>::max();
-
-    if (surfaceSizeUndefined) {
-        int width, height;
-        SDL_GetWindowSize(const_cast<SDL_Window*>(mWindow), &width, &height);
-        mExtent.width = std::clamp<decltype(width)>(width, mSurfaceCapabilities.minImageExtent.width, mSurfaceCapabilities.maxImageExtent.width);
-        mExtent.height = std::clamp<decltype(height)>(height, mSurfaceCapabilities.minImageExtent.height, mSurfaceCapabilities.maxImageExtent.height);
-    } else {
-        mExtent = mSurfaceCapabilities.currentExtent;
-    }
-    return *this;
+               if (surfaceSizeUndefined) {
+                              int width, height;
+                              SDL_GetWindowSize(const_cast<SDL_Window*>(mWindow), &width, &height);
+                              mExtent.width  = std::clamp<decltype(width)>(width, mSurfaceCapabilities.minImageExtent.width, mSurfaceCapabilities.maxImageExtent.width);
+                              mExtent.height = std::clamp<decltype(height)>(height, mSurfaceCapabilities.minImageExtent.height, mSurfaceCapabilities.maxImageExtent.height);
+               } else {
+                              mExtent = mSurfaceCapabilities.currentExtent;
+               }
+               return *this;
 }
