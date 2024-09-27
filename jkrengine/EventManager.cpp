f84999a001278d6194cc3012fc44dc74dd07b2bd
Darshan Koirala <theanimatorspal@gmail.com>
2024-09-27 13:40:36 +0545

    Touch Events done

--- jkrengine/EventManager.cpp
+++ jkrengine/EventManager.cpp
@@ -87,3 +87,56 @@ void Jkr::EventManager::UpdateBoundedRect(uint32_t inId,
 void Jkr::EventManager::SetEventCallBack(const std::function<void()> &inEventCallBack) {
     mEventCallBack.push_back(inEventCallBack);
 }
+
+bool Jkr::EventManager::ShouldQuit() const { return should_quit; }
+SDL_Event Jkr::EventManager::GetEventHandle() const { return mEvent; }
+glm::ivec2 Jkr::EventManager::GetMousePos() const { return mMousePos; }
+glm::ivec2 Jkr::EventManager::GetRelativeMousePos() const { return mRelativePos; }
+int Jkr::EventManager::GetMouseButtonValue() const { return mCurrentPushedMouseButton; }
+
+bool Jkr::EventManager::IsKeyPressedContinous(int inScanCode) const {
+    return (bool)mKeyboardState[inScanCode];
+}
+bool Jkr::EventManager::IsKeyReleased(SDL_Keycode inKey) {
+    return mEvent.type == SDL_KEYUP and mEvent.key.keysym.sym == inKey;
+}
+bool Jkr::EventManager::IsKeyPressed(SDL_Keycode inKey) {
+    return mEvent.type == SDL_KEYDOWN and mEvent.key.keysym.sym == inKey;
+}
+bool Jkr::EventManager::IsLeftButtonPressedContinous() const {
+    return (SDL_BUTTON(SDL_BUTTON_LEFT) bitand this->GetMouseButtonValue()) != 0;
+}
+bool Jkr::EventManager::IsRightButtonPressedContinous() const {
+    return (SDL_BUTTON(SDL_BUTTON_RIGHT) bitand this->GetMouseButtonValue()) != 0;
+}
+bool Jkr::EventManager::IsLeftButtonPressed() const {
+    return (mEvent.type == SDL_MOUSEBUTTONDOWN) && (mEvent.button.button == SDL_BUTTON_LEFT);
+}
+bool Jkr::EventManager::IsRightButtonPressed() const {
+    return (mEvent.type == SDL_MOUSEBUTTONDOWN) && (mEvent.button.button == SDL_BUTTON_RIGHT);
+}
+bool Jkr::EventManager::IsCloseWindowEvent() const { return mCloseWindowEvent; }
+bool Jkr::EventManager::IsFingerDownEvent() const { return mEvent.type == SDL_FINGERDOWN; }
+bool Jkr::EventManager::IsFingerUpEvent() const { return mEvent.type == SDL_FINGERUP; }
+
+glm::vec3 Jkr::EventManager::GetTouchLocation() const {
+    return glm::vec3(mEvent.tfinger.x * mWindowSize.x * mOffscreenByWindowSize.x,
+                     mEvent.tfinger.y * mWindowSize.y * mOffscreenByWindowSize.y,
+                     mEvent.tfinger.pressure);
+}
+glm::vec3 Jkr::EventManager::GetTouchRelativeLocation() const {
+    return glm::vec3(mEvent.tfinger.dx * mWindowSize.x * mOffscreenByWindowSize.x,
+                     mEvent.tfinger.dy * mWindowSize.y * mOffscreenByWindowSize.y,
+                     mEvent.tfinger.pressure);
+}
+
+bool Jkr::EventManager::IsFingerMotionEvent() const { return mEvent.type == SDL_FINGERMOTION; }
+
+uint32_t Jkr::EventManager::GetTouchId() const { return mEvent.tfinger.touchId; }
+
+bool Jkr::EventManager::IsMultiGestureEvent() const { return mEvent.type == SDL_MULTIGESTURE; }
+
+glm::vec4 Jkr::EventManager::GetMultiGesture() const {
+    return glm::vec4(
+         mEvent.mgesture.x, mEvent.mgesture.y, mEvent.mgesture.dDist, mEvent.mgesture.dTheta);
+}
